fadb2e13b7605ecd3b2f772297d5aaeffdc7b771
LannyRivero <lannyrivero288@gmail.com>
2025-03-17 00:15:47 +0100

    feat: update StatisticsSection layout and spacing for better alignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import IntroSection from "../components/IntroSection";
+import StatisticsSection from "../components/StatisticsSection";
+
+// Importa la imagen correctamente si está en `src/assets/`
+import bgImage from "../assets/generacion-eolica.jpg"; 
 
 const Home = () => {
   return (
     <div 
       className="relative min-h-screen w-full bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bgImage})` }}
+      style={{ backgroundImage: `url(${bgImage})` }} // Asegura que la imagen se carga correctamente
     >
-      {/* Capa de superposición para oscurecer la imagen */}
-      <div className="absolute inset-0 bg-black/60 md:bg-black/50 lg:bg-black/40"></div>
+      {/* Superposición para oscurecer el fondo */}
+      <div className="absolute inset-0 bg-black/50"></div>
 
-      <main className="relative z-10 flex flex-col items-center justify-center text-center text-white px-6 pt-20 md:pt-28">
+      {/* Contenido Principal */}
+      <main className="relative z-10 flex flex-col items-center justify-center text-white text-center pt-14">
+
         {/* Sección de Introducción */}
         <IntroSection />
 
-
+        {/* Sección de estadísticas */}
+        <section className="mt-4 w-full max-w-6xl px-6">
+          <StatisticsSection />
+        </section>
+      </main>
     </div>
   );
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
