d58194bc4478dee3d3b492cc20cee45b0974c184
lannyRivero <lannyrivero288@gmail.com>
2025-03-23 19:03:56 +0100

    feat: implement backend connection test in frontend

---
+++ src/components/section/BackendStatus.jsx
@@ -0,0 +1,23 @@
+
+import { useEffect, useState } from "react";
+import checkBackendStatus from "../../services/apiService";
+
+
+const BackendStatus = () => {
+  const [status, setStatus] = useState("Checking backend...");
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      const result = await checkBackendStatus();
+      setStatus(result);
+    };
+
+    fetchStatus();
+  }, []);
+
+  return <div>{status}</div>;
+};
+
+export default BackendStatus;
+
+
