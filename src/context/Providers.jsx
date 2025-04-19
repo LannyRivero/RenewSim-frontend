46e4c3ff8b45b645bb8f9c4fca4341dbfee161b1
lannyRivero <lannyrivero288@gmail.com>
2025-04-19 20:54:30 +0200

    test: add unit tests for UserService (getAllUsers, updateUserRoles, deleteUser)

--- src/context/Providers.jsx
+++ src/context/Providers.jsx
@@ -2,16 +2,14 @@
 import { NotificationProvider } from "./NotificationContext";
 import { SimulationProvider } from "./SimulationContext";
 import { LoadingProvider } from "./LoadingContext.jsx";
-import { ProfileProvider } from './ProfileContext';
+
 
 export const AppProviders = ({ children }) => (
 
   <NotificationProvider>
     <SimulationProvider>
       <LoadingProvider>
-        <ProfileProvider>
           {children}
-        </ProfileProvider>
       </LoadingProvider>
     </SimulationProvider>
   </NotificationProvider>
