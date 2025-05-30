e1861a4f84e92e2205d8f12d104dca9103d831fe
lannyrivero <lannyrivero288@gmail.com>
2025-05-30 08:42:59 +0200

    test(auth): add browser-level validation checks for empty email and password

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -12,5 +12,23 @@ describe('Login Flow', () => {
     cy.get('button[type=submit]').click();
     cy.contains('Error del servidor. Inténtalo más tarde.').should('be.visible');   
   })
+
+  it('should show browser validation for empty fields', () => {
+ 
+  cy.get('button[type="submit"]').click();
+  
+  cy.get('input[name="username"]').then(($emailInput) => {
+    expect($emailInput[0].checkValidity()).to.be.false;
+  });
+
+  cy.get('input[name="username"]').type('lannyrivero288@gmail.com');
+
+  cy.get('button[type="submit"]').click();
+
+  cy.get('input[name="password"]').then(($passwordInput) => {
+    expect($passwordInput[0].checkValidity()).to.be.false;
+  });
+});
+
  
 });
