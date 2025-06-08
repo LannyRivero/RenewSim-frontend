251ba04dd9e63999a8dbd03fe771ac5fed3c3730
lannyrivero <lannyrivero288@gmail.com>
2025-06-08 16:13:24 +0200

    feat(simulation): refactor enterData test to use dynamic simulation data

--- cypress/e2e/simulation/enterData.cy.js
+++ cypress/e2e/simulation/enterData.cy.js
@@ -1,36 +1,36 @@
 describe('Energy Simulation Flow - Datos válidos', () => {
 
-beforeEach(() => {
- cy.fixture('users').then((users) => {
+  beforeEach(() => {
+    cy.fixture('users').then((users) => {
       const user = users.daniel;
       cy.loginViaUI(user.email, user.password);
     });
-});
+  });
 
+  it('should allow entering valid data and display correct results', () => {
 
-it('should allow entering valid data and display correct results', () => {
-  cy.get('form').should('be.visible');
-  cy.fillSimulationForm('Gijón', '100', 'solar'); 
-  cy.contains('h3', 'Simulation Results').should('be.visible');
-  
-  const expectedResults = [
-      'Energia Generada al Año',
-      'Ahorros Estimados',
-      'Retorno de Inversión'
-    ];
+    cy.fixture('simulationData').then((data) => {
+      const { location, consumption, energyType } = data.validSimulation;
 
-    expectedResults.forEach((resultText) => {
-      cy.contains(resultText).should('be.visible');
+      cy.get('form').should('be.visible');
+      cy.fillSimulationForm(location, consumption, energyType);
+      cy.contains('h3', 'Simulation Results').should('be.visible');
 
-      cy.log('Flujo de simulación completado con éxito y resultados verificados');
+      const expectedResults = [
+        'Energia Generada al Año',
+        'Ahorros Estimados',
+        'Retorno de Inversión'
+      ];
+
+      expectedResults.forEach((resultText) => {
+        cy.contains(resultText).should('be.visible');
+      });
+
+      cy.log('✅ Flujo de simulación completado con éxito y resultados verificados');
     });
+  });
+  
 });
 
-
-
-
-
-
-});
 
 
